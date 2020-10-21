6c07e7eb2769d456545fc577656bf834b2b6ab58
DanTheMan114 <danielturner184@gmail.com>
2020-10-21 15:12:36 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,14 @@ class Student < User
         @knowledge = []
     end
 
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
+
 end
